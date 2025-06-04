3c15c3a09628dcbd987b30ebf60c3c1ace4ebd86
tenseika <andrmltng@gmail.com>
2025-06-04 11:56:04 +0800

    Game Over

--- Flap n Fact/Assets/Scripts/GameManager.cs
+++ Flap n Fact/Assets/Scripts/GameManager.cs
@@ -4,11 +4,22 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Tambahkan referensi ke UI Score
+    public GameObject GOUI;
+    public TextMeshProUGUI finalScoreText;
+    public GameObject[] UIObjects;
     private int score;
 
     private void Start()
     {
         UpdateScoreUI();
+    }
+
+    public void HideAllUIObjects()
+    {
+        foreach (GameObject uiObject in UIObjects)
+        {
+            uiObject.SetActive(false);
+        }
     }
 
     public void IncreaseScore()
@@ -19,7 +30,11 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        Debug.Log("Game Over! Final Score: " + score);
+        //Debug.Log("Game Over! Final Score: " + score);
+        Time.timeScale = 0f;
+        GOUI.SetActive(true);
+        HideAllUIObjects();
+        finalScoreText.text = "" + score;
     }
 
     private void UpdateScoreUI()
