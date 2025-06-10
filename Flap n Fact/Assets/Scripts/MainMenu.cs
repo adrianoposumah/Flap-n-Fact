8e6ca2b7a366d322ab7a7f398d668275c362b252
tenseika <andrmltng@gmail.com>
2025-06-10 23:42:00 +0800

    adjust music slider

--- Flap n Fact/Assets/Scripts/MainMenu.cs
+++ Flap n Fact/Assets/Scripts/MainMenu.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider masterSlider;
+
+    public AudioMixer audioMixer;
     private void Start()
     {
+        LoadVolume();
         MusicManager.Instance.PlayMusic("MainMenu");
     }
 
@@ -18,5 +24,21 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("masterVolume", out float masterVolume);
+        PlayerPrefs.SetFloat("masterVolume", masterVolume);
+    }
+
+    public void LoadVolume()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
     }
 }
